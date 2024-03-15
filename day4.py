6a890bcae57249c5cbd8902ca979902bb2caea19
Arshadshaik2003 <162983725+Arshadshaik2003@users.noreply.github.com>
2024-03-15 09:23:37 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,92 @@
+
+i = 20
+while i<31:
+    if i==27:
+       continue
+    print(i)
+    i=i+1
+
+ i = 20
+ while i<31:
+    print(i)
+   i+=1
+ if i==27:
+     break
+
+ i = 20
+ while i<31:
+     print(i)
+     if i==27:
+         break
+     i+=1
+
+ ! ------> continue
+
+ i = 20
+ while i<31:
+     if i==27:
+        continue
+     print(1)
+     i=i+1
+
+i = 20
+while i<31:
+     i=i+1
+     if i==27:
+         continue
+     print(i)
+
+i =12
+sum=0
+while i<23:
+    sum=sum+i
+    i+=1
+print(sum)
+
+i = 20
+sum = 0
+count = 0
+while i<=30:
+    sum = sum+i
+    count+=1
+    i+=1
+print(sum/count)
+
+
+
+for row in range(rows):
+for col in range(cols):
+print("*", end=" ")
+print()
+
+
+sum = 0
+for row in range(5):
+    for col in range(5):
+        sum sum+1
+        print(sum, end=" ")
+    print()
+
+
+
+to print stars in right angled triangle
+
+for row in range(0, 6)
+    for col in range(0, row+1):
+        print("*", end="")
+    print()
+
+ * * * * *
+ *       *
+ *       *
+ *       *
+ * * * * *
+
+
+
+
+
+
+
+
+
